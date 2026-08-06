1df63bc1ae9ef7230641144e4536849356987251
Nguyen Dao Nam Hai <namhai23092005@gmail.com>
2026-08-06 18:04:27 +0700

    chore(backend): remove temporary test scripts

--- list_models.py
+++
@@ -1,7 +0,0 @@
-import os
-from google import genai
-
-client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY_1"))
-for m in client.models.list():
-    if "embed" in m.name:
-        print(m.name, m.supported_actions)
